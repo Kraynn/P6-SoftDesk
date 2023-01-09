fbf16395b2daa9ee1dfc8d0637d9c37a69e4d7e8
Kraynn <75184473+Kraynn@users.noreply.github.com>
2023-01-09 17:46:59 +0100

    Update serializers.py

--- app/softdesk/serializers.py
+++ app/softdesk/serializers.py
@@ -6,6 +6,39 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from softdesk.models import Projects, Comments, Issues 
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "password",
+            "email",
+            "first_name",
+            "last_name",
+        )
+        extra_kwargs = {'password': {'write_only': True}}
+
+        def validate_name(self, value):
+            if User.objects.filter(username=value).exists():
+                raise serializers.ValidationError('Ce nom existe déjà!')
+            return value
+
+        def create(self, data):
+            user = User.objects.create(
+                username=data["username"],
+                email=data["email"],
+                first_name=data["first_name"],
+                last_name=data["last_name"],
+            )
+            user.set_password(data["password"])
+            user.save()
+
+            return user
  
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
